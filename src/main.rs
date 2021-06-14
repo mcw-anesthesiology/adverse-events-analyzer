0fd3ceb0d13f5c904c6a6f296934528ddd0ab6eb
Jacob Mischka <jacob@mischka.me>
2021-06-14 11:40:15 -0500

    Improve adverse-events CLI

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
-use adverse_events::{AdverseEvents, AdverseEventsView};
+use clap::{App, Arg, SubCommand};
+use csv;
 
-use std::{env, fs::File};
+use adverse_events::{sort_map, AdverseEvents, AdverseEventsView};
+
+use std::{ffi::OsStr, fs::File, io, path::Path};
 
 fn main() {
-    let path = env::args()
-        .skip(1)
-        .next()
-        .expect("must pass ZIP path as first argument");
+    let matches = App::new("Adverse events analyzer")
+        .arg(
+            Arg::with_name("input")
+                .help("Input record (either CSV, or ZIP archive with a single CSV inside)")
+                .required(true),
+        )
+        .subcommand(SubCommand::with_name("counts"))
+        .get_matches();
 
+    let path = Path::new(matches.value_of("input").unwrap());
     let file = File::open(path).unwrap();
 
-    let adverse_events = AdverseEvents::from_zip(file).unwrap();
+    let adverse_events = match path.extension().and_then(OsStr::to_str) {
+        Some("zip") => AdverseEvents::from_zip(file),
+        Some("csv") => AdverseEvents::from_csv_reader(file),
+        Some(ext) => panic!("unsupported extension: {}", ext),
+        None => panic!("input path missing extension"),
+    }
+    .unwrap();
 
     let view: AdverseEventsView = adverse_events.view();
 
-    for (event, count) in view.event_counts() {
-        println!("{}: {}", event, count);
+    match matches.subcommand() {
+        ("counts", _) => {
+            event_counts(&view).unwrap();
+        }
+        (command, _) => {
+            panic!("unknown subcommand {}", command);
+        }
     }
+}
+
+fn event_counts(view: &AdverseEventsView<'_>) -> Result<(), csv::Error> {
+    let mut writer = csv::Writer::from_writer(io::stdout());
+
+    writer.write_record(&["Adverse event", "Count"])?;
+
+    let mut total: u64 = 0;
+    for (event, count) in sort_map(view.event_counts()) {
+        total += count as u64;
+        writer.write_record(&[event, &count.to_string()])?;
+    }
+
+    writer.write_record(&["Total", &total.to_string()])?;
+
+    Ok(())
 }
