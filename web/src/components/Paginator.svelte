29d8479f2839912b4e18ec2a55f3873edad83e11
Jacob Mischka <jacob@mischka.me>
2021-06-18 13:37:14 -0500

    Improve RecordsList to show paginated table of records

---
+++ web/src/components/Paginator.svelte
@@ -0,0 +1,63 @@
+<div class="paginator">
+	<button type="button" on:click={handleBackPage} disabled={pageNum === 0}>
+		←
+	</button>
+
+	<span>
+		Page
+
+		<input type="number" aria-label="Page number" value={pageNum + 1} on:change={handlePageChange} min="1" max={maxPageNum + 1} />
+
+		of
+
+		<span>
+			{maxPageNum + 1}
+		</span>
+	</span>
+
+	<button type="button" on:click={handleForwardPage} disabled={pageNum >= maxPageNum}>
+		→
+	</button>
+</div>
+
+<script>
+	export let pageNum, maxPageNum;
+
+	function handlePageChange(event) {
+		const input = Number(event.target.value);
+		if (!Number.isNaN(input)) {
+			const value = input - 1;
+			if (value >= 0 && value <= maxPageNum) {
+				pageNum = value;
+			}
+		}
+	}
+
+	function handleBackPage() {
+		pageNum--;
+	}
+
+	function handleForwardPage() {
+		pageNum++;
+	}
+</script>
+
+<style>
+	.paginator {
+		margin: 1em;
+		display: flex;
+		justify-content: center;
+	}
+
+	.paginator > * {
+		margin: 0.5em;
+	}
+
+	button {
+		padding: 0.25em 1em;
+	}
+
+	input {
+		width: 5em;
+	}
+</style>
--- web/src/components/RecordsList.svelte
+++ web/src/components/RecordsList.svelte
@@ -1,18 +1,103 @@
-<ol>
-	{#each records as record}
-		<li><pre>{JSON.stringify(record)}</pre></li>
-	{/each}
-</ol>
+<div>
+
+	<Paginator bind:pageNum {maxPageNum} />
+
+	<table>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>MRN</th>
+				<th>Diagnosis</th>
+				<th>Procedure</th>
+				<th>Anesthesiologist</th>
+				<th>Anesthesia staff</th>
+				<th>Location</th>
+				<th>ASA</th>
+				<th>Start</th>
+				<th>Stop</th>
+				<th>Smoker</th>
+				<th>Age</th>
+				<th>BMI</th>
+				<th>Adverse events</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each pageRecords as record}
+				<tr>
+					<td>
+						<RichDate date={record.date} />
+					</td>
+					<td>{record.mrn}</td>
+					<td>{record.diagnosis}</td>
+					<td>{record.procedure}</td>
+					<td>{record.anesthesiologist}</td>
+					<td>
+						<ul>
+							{#each record.anesthesiaStaff as staffMember}
+								<li>{staffMember}</li>
+							{/each}
+						</ul>
+					</td>
+					<td>{record.location}</td>
+					<td>{record.asa}</td>
+					<td>{record.anStart}</td>
+					<td>{record.anStop}</td>
+					<td>
+						{#if record.smoker}
+							✓
+						{/if}
+					</td>
+					<td>{record.age}</td>
+					<td>{record.bmi}</td>
+					<td>
+						<ul>
+							{#each record.adverseEvents as event}
+								<li>{event}</li>
+							{/each}
+						</ul>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+
+	<Paginator bind:pageNum {maxPageNum} />
+
+	<div>
+		<label>
+			Page size
+			<input type="number" bind:value={pageSize} step="5" />
+		</label>
+	</div>
+</div>
 
 <script>
+	import RichDate from './RichDate.svelte';
+	import Paginator from './Paginator.svelte';
+
 	import { getRecords } from '../wasm-wrapper.js';
 
 	export let viewHandle;
-	let records = [];
 
+	let records = [];
 	$: fetchRecords(viewHandle);
 
 	async function fetchRecords(handle) {
 		records = await getRecords(handle);
 	}
+
+	let pageNum = 0;
+	let pageSize = 20;
+	let pageRecords = [];
+	let maxPageNum;
+	$: maxPageNum = Math.ceil(records.length / pageSize) - 1;
+
+	$: pageRecords = records.slice(pageNum * pageSize, (pageNum + 1) * pageSize);
+
 </script>
+
+<style>
+	ul {
+		padding-left: 1em;
+	}
+</style>
